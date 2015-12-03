84d6ec321bb38b2fdb0df395e3ad7b0c9f0f58c9
wlong2017 <wlong123>
2015-12-03 14:24:54 -0500

    done

--- GameBoard.java
+++ GameBoard.java
@@ -1,58 +1,107 @@
+/**
+	GameBoard Class for Sudoku
+	@version 12/3/15
+	@author Will Long
+*/
 public class GameBoard
 {
+	/**2d array that contains the numbers in the board*/
 	private int[][] board;
 	
+	/**
+	default constructor. copies in data from "Sheet1.CSV" and creates a new game board with the data from the CSV file
+	*/
 	public GameBoard()
 	{
 		int i = 0;
 		board = new int[9][9];
-		String s = ReadCSV.readCSV();
+		String s = ReadCSV.readCSV();  //data in CSV file is turned into a string
 		for(int r = 0; r < board.length; r++)
 		{
 			for(int c = 0; c < board.length; c++)
 			{
-				while((s.charAt(i) == ',') || (s.charAt(i) == '\n'))
+				while((s.charAt(i) == ',') || (s.charAt(i) == '\n'))  //passes over enters and commas
 				{
 					i++;
 				}
-				if(s.charAt(i) == '-')
+				if(s.charAt(i) == '-')  //dashes represent 0
 					board[r][c] = 0;
 				else
-					board[r][c] = (int)s.charAt(i) - 48;
+					board[r][c] = (int)s.charAt(i) - 48;  //int value of the character which is a number 
 				i++;
 			}
 		}
 	}
 	
+	/**
+	copy constructor for game board
+	*/
+	public GameBoard(GameBoard b)
+	{
+		board = new int[9][9];
+		for(int r = 0; r < board.length; r++)
+		{
+			for(int c = 0; c < board[0].length; c++)
+			{
+				board[r][c] = b.get(r,c);
+			}
+		}
+	}
+	
+	/**
+	places an inputted number at inputted spot in the game board
+	@param r row of the number to be placed
+	@param c column of the number to be placed
+	@param n number to be placed 
+	*/
 	public void place(int r, int c, int n)
 	{
 		board[r][c] = n;
 	}
 	
+	/**
+	returns a value in the game board
+	@param r row of the value that is going to be returned
+	@param c column of the value that is going to be returned
+	@return int value of board at the inputted row and column
+	*/
 	public int get(int r, int c)
 	{
 		return board[r][c];
 	}
 	
+	/**
+	removes a value in the board by setting it to 0
+	@param r row of the value that is going to be removed
+	@param c column of the value that is going to be removed
+	*/
 	public void remove(int r, int c)
 	{
 		board[r][c] = 0;
 	}
 	
+	/**
+	checks whether an inputted number can be placed at an inputted spot on the board
+	@param r row of the spot that is going to be checked
+	@param c column of the spot that is going to be checked
+	@param n number that is going to be checked
+	@return boolean returns true if n can be placed at the spot, false if not
+	*/
 	public boolean canPlace(int r, int c, int n)
 	{
-		if(board[r][c] != 0)
+		if(board[r][c] != 0)  //don't want to replace a spot at the board that isn't 0
 			return false;
-		for(int row = 0; row < board.length; row++)
+		for(int row = 0; row < board.length; row++)  //checks rows
 		{
 			if(board[row][c] == n)
 				return false;
 		}
-		for(int col = 0; col < board.length; col++)
+		for(int col = 0; col < board.length; col++)  //checks columns
 		{
 			if(board[r][col] == n)
 				return false;
 		}
+		//finds starts of the 3x3 box of n
 		int row = 0;
 		int col = 0;
 		if(r > 2)
@@ -74,26 +123,30 @@ public class GameBoard
 		return true;
 	}
 	
+	/**
+	finds the most constrained spot on the board. returns the row and column of the most constrained spot
+	@return int[] int array containing the row and column of the most constrined spot
+	*/
 	public int[] mostConstrained()
 	{
-		int lowest = 123456;
+		int lowest = 123456; //initialized with very high number
 		int[] mostConstrained = new int[2];
 		for(int r = 0; r < board.length; r++)
 		{
 			for(int c = 0; c < board.length; c++)
 			{
-				if(board[r][c] == 0)
+				if(board[r][c] == 0) //only checks spots on board that are 0
 				{
-					int count = 0;
-					for(int i = 1; i < 10; i++)
+					int count = 0;  //counts the number of numbers that can be placed at s spot
+					for(int i = 1; i < 10; i++) //goes through numbers 1-9 and checks if the numbers can be placed at the spot
 					{
 						if(canPlace(r,c,i))
 							count++;
 					}
-					if(count < lowest)
+					if(count < lowest)  
 					{
 						lowest = count;
-						mostConstrained[0] = r;
+						mostConstrained[0] = r;  
 						mostConstrained[1] = c;
 					}
 				}
@@ -102,6 +155,10 @@ public class GameBoard
 		return mostConstrained;
 	}
 	
+	/**
+	determines whether or not the board is solved
+	@return boolean returns true if the board is solved, false if not
+	*/
 	public boolean solved()
 	{
 		for(int row = 0; row < board.length; row++)
@@ -115,6 +172,10 @@ public class GameBoard
 		return true;
 	}
 	
+	/**
+	returns string representation of the board
+	@return String returns string representation of the board
+	*/
 	public String toString()
 	{
 		String s = "";
@@ -124,7 +185,7 @@ public class GameBoard
 			{
 				s += "|" + board[r][c];
 			}				
-			s += "|" + "\n" + "-------------" + "\n";
+			s += "|" + "\n" + "-------------------" + "\n";
 		}
 		return s;
 	}
--- Solver.java
+++ Solver.java
@@ -1,28 +1,37 @@
+/**
+	Solver Class for a Sudoku Board
+	@version 12/3/15
+	@author Will Long
+*/
 public class Solver<E>
 {
+	/**
+	main method for solver. solves a sudoku puzzle
+	*/
 	public static void main(String [] args)
 	{
-		Stack<GameBoard> stack = new LinkedList<GameBoard>();
-		GameBoard board = new GameBoard();
+		Stack<GameBoard> stack = new LinkedList<GameBoard>(); //intializes stack
+		GameBoard board = new GameBoard(); //creates the game board
 		stack.push(board);
 		boolean solved = false;	
 		while(solved == false)
 		{
-			if(stack.isEmpty())
+			if(stack.isEmpty()) //if stack is empty the board is unsolvable
 				System.out.println("board is unsolvable");
-			GameBoard curr = stack.pop();
-			if(curr.solved())
+			GameBoard curr = stack.pop(); 
+			if(curr.solved()) 
 			{
 				System.out.println(curr);
 				solved = true;
+				return;
 			}
-			int[] mostConstrained = curr.mostConstrained();
-			for(int i = 1; i < 10; i++)
+			int[] mostConstrained = curr.mostConstrained(); //array containing the row and column of the most constrined spot on the board
+			for(int i = 1; i < 10; i++) //checks what numbers can be placed at the most constrained spot
 			{
 				if(curr.canPlace(mostConstrained[0], mostConstrained[1], i))
 				{
-					GameBoard temp = curr;
-					temp.place(mostConstrained[0],mostConstrained[1], i);
+					GameBoard temp = new GameBoard(curr); //calls the copy constructor and creates a copy of current game board
+					temp.place(mostConstrained[0],mostConstrained[1], i); 
 					stack.push(temp);
 				}
 			}
